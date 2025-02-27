243d7d2a8e255adc71209ed1d83e050802d528e1
yogesh shelke <shelkeyogesh874@gmail.com>
2025-02-27 15:34:18 +0530

    index.ts update

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "dist")))
 app.use("/api", userRoutes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.sendFile(path.join(__dirname, "dist/index.html"))
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
     // res.status(404).json({ message: "Route Not Found" })
 })
 
